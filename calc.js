ab95651e19260e51d50511bc5d09ca762251e90f
alien_nt <alienvsssh@gmail.com>
2022-06-16 19:17:54 +0300

    New calculator

---
+++ calc.js
@@ -0,0 +1,45 @@
+function Calc(operation, a, b) {
+    if (typeof operation !== 'string' || operation == '' || typeof a !== 'number' || typeof b !== 'number') {
+        console.log('error')
+    }
+    if (operation === 'sum') {
+        let result = a + b;
+        console.log(result)
+    } else if (operation === 'minus') {
+        let result = a - b;
+        console.log(result)
+    } else if (operation === 'div') {
+        if (a == 0, b == 0) {
+            console.log('на нуль делить нельзя')
+    }
+        let result = a / b;
+        console.log(result)
+    } else if (operation === 'multi') {
+        let result = a * b;
+        console.log(result)
+    } else {
+        return ('unknown operation')
+    }
+    
+        switch (operation) {
+        case 'sum':
+        console.log (a+b);
+        break;
+        case 'minus':
+        console.log (a-b);
+        break;
+        case 'div':
+        console.log (a / b);
+        break;
+        case 'multi':
+        console.log (a * b);
+        break;
+    }
+
+}
+
+
+Calc('sum', 1, 2);
+Calc('minus', 1, 2);
+Calc('div', 1, 0)
+Calc('multi', 1, 2)
